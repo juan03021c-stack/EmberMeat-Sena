f4281a96ecddfe34b700be94b53591157033c6c3
jean-git <jeanpool60778@gmail.com>
2026-08-11 08:26:04 -0500

    Resuelvo comflictos y agrego home y login

--- ember-meats-admin/src/App.jsx
+++ ember-meats-admin/src/App.jsx
@@ -1,6 +1,78 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Dashboard from './pages/Dashboard' 
-import Products  from './pages/Products' 
-import Orders    from './pages/Orders' 
-import Users     from './pages/Users' 
+import Sidebar from './components/Sidebar'
+import Dashboard from './pages/Dashboard'
+import Products from './pages/Products'
+import Orders from './pages/Orders'
+import Users from './pages/Users'
+import Login from './pages/Login'
+import Home from './pages/Home'
+
+function AdminLayout({ children }) {
+  return (
+    <div className="d-flex">
+      <Sidebar />
+
+      <div
+        className="flex-grow-1"
+        style={{
+          background: '#F5F3EE',
+          minHeight: '100vh'
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/"
+          element={
+            <AdminLayout>
+              <Dashboard />
+            </AdminLayout>
+          }
+        />
+
+        <Route
+          path="/products"
+          element={
+            <AdminLayout>
+              <Products />
+            </AdminLayout>
+          }
+        />
+
+        <Route
+          path="/orders"
+          element={
+            <AdminLayout>
+              <Orders />
+            </AdminLayout>
+          }
+        />
+
+        <Route
+          path="/users"
+          element={
+            <AdminLayout>
+              <Users />
+            </AdminLayout>
+          }
+        />
+
+      </Routes>
+    </Router>
+  )
+}
 
