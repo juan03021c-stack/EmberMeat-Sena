22f4dddff380ff6033cb61650ab14a2131cd336c
Diego <juan03021c@gmail.com>
2026-09-11 10:48:36 -0500

    organice estilos en el catalogo

--- ember-meats-admin/src/pages/Catalogo.jsx
+++ ember-meats-admin/src/pages/Catalogo.jsx
@@ -1,4 +1,5 @@
 import React, { useMemo, useState, useEffect } from "react";
+import { NavLink } from 'react-router-dom';
 import "../assets/EmberMeat.css";
 import { obtenerProductos, obtenerCategorias, URL_BASE } from '../services/Api';
 import { useCarrito } from '../components/CarritoContext';
@@ -213,6 +214,7 @@ export default function Catalogo() {
                       );
                     })
                   )}
+                 
                 </div>
 
                 {/* Disponibilidad */}
@@ -240,6 +242,7 @@ export default function Catalogo() {
                     />
                     <label className="form-check-label" htmlFor="disp-solo">Solo disponibles</label>
                   </div>
+                 <NavLink to="/" className="btn btn-secondary me-2">Regresar al inicio</NavLink>
                 </div>
               </div>
             </div>
